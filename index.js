364b19a4ddb26a768f9acfb15573c10bc8071987
Aaron Hellam <aaronhellam@gmail.com>
2022-02-24 14:17:25 -0800

    commit tree 63c46a9d9749134daf207c77aeb7eb69b2486a37

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = 'bob'; 
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+// console.log(upperCaseCustomerName())
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+    console.log(bestCustomer);
+}
+// setBestCustomer()
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+const leastFavoriteCustomer = 'karen'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'chad'
+}
